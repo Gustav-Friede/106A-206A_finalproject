db82d63652534da77e86de64c758fd1118607126
Mario Gonzalez <mario.gonzalez@berkeley.edu>
2024-12-05 15:21:26 -0800

    finished extracting maze

--- computer_vision/edge_detect.py
+++ computer_vision/edge_detect.py
@@ -2,6 +2,7 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
 filepath = '../imgs/'
 
@@ -10,40 +11,84 @@ img = cv.imread(filepath + 'calibrated.jpg', cv.IMREAD_REDUCED_GRAYSCALE_8)
 
 
 edges = cv.Canny(img, 64, 264)
-corners = cv.goodFeaturesToTrack(edges, 64, 0.02, 36)
+corners = cv.goodFeaturesToTrack(edges, 32,0.08, 50)
+print(corners)
 corners = np.int0(corners)
 
 img_wc = edges.copy()
+krn = cv.getStructuringElement(cv.MORPH_RECT, (16, 16))
+dlt = cv.dilate(img_wc, krn, iterations=5)
+res = 255 - cv.bitwise_and(dlt, img_wc)
+
+
 for corner in corners:
     x, y = corner.ravel()
-    cv.circle(img_wc, (int(x), int(y)), 5, 255, -1)
+    cv.circle(res, (int(x), int(y)), 5, 0, -1)
 
-plt.figure(figsize=(12, 12))
+image = cv.findContours(img_wc, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+print(image)
+#plt.imshow(image)
 
-# original image
-plt.subplot(2, 2, 1)
-plt.imshow(cv.imread(filepath + 'calibrated.jpg'), cmap='gray')
+# cv.imshow('edge and corners', img_wc)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+# Display original and edge-detected images
+plt.figure(figsize=(10, 5))  # Adjust figure size for better visualization
+#
+#Subplot 1: Original Image
+plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+plt.imshow(img, cmap='gray')
 plt.title('Original Image')
-plt.xticks([]), plt.yticks([])
+#plt.xticks([]), plt.yticks([])
 
-# grayscale image
-plt.subplot(2, 2, 2)
-plt.imshow(cv.imread(filepath + 'calibrated.jpg', cv.IMREAD_REDUCED_GRAYSCALE_8), cmap='gray')
-plt.title('Grayscale')
-plt.xticks([]), plt.yticks([])
-
-# canny edge detection
-plt.subplot(2, 2, 3)
-plt.imshow(edges, cmap='gray')
-plt.title('Edges')
-plt.xticks([]), plt.yticks([])
-
-# harris corner detection
-plt.subplot(2, 2, 4)
-plt.imshow(img_wc, cmap='gray')
-plt.title('Corners')
-plt.xticks([]), plt.yticks([])
+# Subplot 2: Edge Image
+plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+plt.imshow(res, cmap='gray')
+plt.title('Edge and Corners')
+#plt.xticks([]), plt.yticks([])
 
 plt.tight_layout()
 plt.show()
+
+
+
+
+
+# plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+# plt.imshow(edges, cmap='gray')
+# plt.title('Canny Edge Detection')
+# plt.xticks([]), plt.yticks([])
+#
+#
+
+
+
+# plt.figure(figsize=(12, 12))
+# #original image
+# plt.subplot(2, 2, 1)
+# plt.imshow(cv.imread(filepath + 'calibrated.jpg'), cmap='gray')
+# plt.title('Original Image')
+# plt.xticks([]), plt.yticks([])
+#
+# #grayscale image
+# plt.subplot(2, 2, 2)
+# plt.imshow(cv.imread(filepath + 'calibrated.jpg', cv.IMREAD_REDUCED_GRAYSCALE_8), cmap='gray')
+# plt.title('Grayscale')
+# plt.xticks([]), plt.yticks([])
+#
+# #canny edge detection
+# plt.subplot(2, 2, 3)
+# plt.imshow(edges, cmap='gray')
+# plt.title('Edges')
+# plt.xticks([]), plt.yticks([])
+#
+# # harris corner detection
+# plt.subplot(2, 2, 4)
+# plt.imshow(img_wc, cmap='gray')
+# plt.title('Corners')
+# plt.xticks([]), plt.yticks([])
+# #
+# plt.tight_layout()
+# plt.show()
 
--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import numpy as np
+
 
 def resizeImage(image, width=None, height=None, inter=cv.INTER_AREA):
     (h, w) = image.shape[:2]
@@ -12,3 +14,29 @@ def resizeImage(image, width=None, height=None, inter=cv.INTER_AREA):
         dim = (width, int(h * r))
 
     return cv.resize(image, dim, interpolation=inter)
+
+def correct_skew(image, delta=1, limit=5):
+    def determine_score(arr, angle):
+        data = inter.rotate(arr, angle, reshape=False, order=0)
+        histogram = np.sum(data, axis=1, dtype=float)
+        score = np.sum((histogram[1:] - histogram[:-1]) ** 2, dtype=float)
+        return histogram, score
+
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+
+    scores = []
+    angles = np.arange(-limit, limit + delta, delta)
+    for angle in angles:
+        histogram, score = determine_score(thresh, angle)
+        scores.append(score)
+
+    best_angle = angles[scores.index(max(scores))]
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv.getRotationMatrix2D(center, best_angle, 1.0)
+    corrected = cv.warpAffine(image, M, (w, h), flags=cv.INTER_CUBIC,
+            borderMode=cv.BORDER_REPLICATE)
+
+    return best_angle, corrected
